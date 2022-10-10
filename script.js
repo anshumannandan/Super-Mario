3a6a5afcd75bed6de8c09dbff4d541e7bf1baec0
anshumannandan <anshuman21xxx034@akgec.ac.in>
2022-10-11 01:58:12 +0530

    fixed infinite jumping

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ const context = gamespace.getContext('2d');
 gamespace.height = window.innerHeight;
 gamespace.width = window.innerWidth;
 var g = 0.5;
+let booljump = true;
 class Character {
     constructor() {
-        this.velocity = { x: 0, y: 5 };
+        this.velocity = { x: 0, y: 0 };
         this.position = { x: 100, y: 600 };
-        this.frame = { x: 0, speed: 0, start: 0, end: 1}
+        this.frame = { x: 0, start: 0, end: 1 }
+        this.speed = 0;
         this.charimg = new Image();
         this.charimg.src = "images/standright.png";
     }
     removeduplicate() {
-        this.frame.speed++;
-        if (this.frame.speed % 30 == 0) {
+        this.speed++;
+        if (this.speed % 30 == 0) {
             this.frame.x++;
         }
         if (this.frame.x > this.frame.end) {
@@ -21,82 +23,81 @@ class Character {
         }
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
-        if (this.position.y+this.velocity.y<=gamespace.height) {
+        if (this.position.y + this.velocity.y <= gamespace.height) {
             this.velocity.y += g;
         }
         else {
             this.velocity.y = 0;
         }
+        if (this.position.y == 649.5) {
+            booljump = true;
+        }
         context.drawImage(this.charimg, 35 * this.frame.x, 0, 35, 38, this.position.x, this.position.y, 60, 60);
     }
 }
-class Base{
-    constructor({x,y},width,height)
-    {
-        this.position={
-            x,y
+class Base {
+    constructor({ x, y }, width, height) {
+        this.position = {
+            x, y
         }
-        this.width=width;
-        this.height=height;
+        this.width = width;
+        this.height = height;
     }
-    draw()
-    {
-        context.fillRect(this.position.x,this.position.y,this.width,this.height);
-        context.fillStyle='green';
+    draw() {
+        context.fillRect(this.position.x, this.position.y, this.width, this.height);
+        context.fillStyle = 'green';
         context.fill();
     }
-} 
-const object=new Character();  
-const multibase=[new Base({x:100,y:400},200,20),new Base({x:400,y:200},200,20),new Base({x:700,y:100},200,20),new Base({x:1100,y:200},200,20),new Base({x:-60,y:700},700,120)
-,new Base({x:1500,y:520},300,20), new Base({x:1900,y:320},900,720), new Base({x:3100,y:520},250,20)]; 
-const keys={
-    right:{
-        pressed:false
+}
+const object = new Character();
+const multibase = [new Base({ x: 100, y: 400 }, 200, 20), new Base({ x: 400, y: 200 }, 200, 20), new Base({ x: 700, y: 100 }, 200, 20), new Base({ x: 1100, y: 200 }, 200, 20), new Base({ x: -60, y: 700 }, 700, 120)
+    , new Base({ x: 1500, y: 520 }, 300, 20), new Base({ x: 1900, y: 320 }, 900, 720), new Base({ x: 3100, y: 520 }, 250, 20)];
+const keys = {
+    right: {
+        pressed: false
     },
-    left:{
-        pressed:false
+    left: {
+        pressed: false
     }
 }
-function Move(){
-    
+function Move() {
+
     requestAnimationFrame(Move);
     context.clearRect(0, 0, gamespace.width, gamespace.height);
     context.beginPath();
     object.removeduplicate();
-    multibase.forEach((base)=> {
+    multibase.forEach((base) => {
         base.draw();
-     })
+    })
     // if(keys.right.pressed && object. position.x<400)
     //     object.velocity.x=5;
     // else if(keys.left.pressed && object.position.x>0)
     //     object.velocity.x=-5;
     // else 
     //     object.velocity.x=0;  
-    
-    if(keys.right.pressed)
-    {
-        multibase.forEach((base)=> {
+
+    if (keys.right.pressed) {
+        multibase.forEach((base) => {
             base.draw();
-            base.position.x+=5;
-         })
-    } 
-    else if(keys.left.pressed)
-    { 
-        multibase.forEach((base)=> {
+            base.position.x += 5;
+        })
+    }
+    else if (keys.left.pressed) {
+        multibase.forEach((base) => {
             base.draw();
-            base.position.x-=5;
-         })
-         
+            base.position.x -= 5;
+        })
     }
 
 
-    
-    multibase.forEach((base)=> {
-    if(object.position.y+50<=base.position.y && object.position.y+50+object.velocity.y>=base.position.y && object.position.x+33>=base.position.x && object.position.x<=base.position.x+base.width)
-    {
-        object.velocity.y=0; 
-    }})
-    
+
+    multibase.forEach((base) => {
+        if (object.position.y + 50 <= base.position.y && object.position.y + 50 + object.velocity.y >= base.position.y && object.position.x + 33 >= base.position.x && object.position.x <= base.position.x + base.width) {
+            object.velocity.y = 0;
+            booljump = true;
+        }
+    })
+
 }
 Move();
 
@@ -105,14 +106,18 @@ let boolleftrun = true;
 document.onkeydown = (e) => {
     e = e || window.event;
     if (e.key === 'ArrowUp') {
-        object.velocity.y = -15;
+        if (booljump) {
+            object.velocity.y = -20;
+            booljump = false
+        }
         // } else if (e.key === 'ArrowDown') {
-        // object.radius=5;
+        // 
     } else if (e.key === 'ArrowLeft') {
         object.velocity.x = -5;
-        if (boolleftrun){
-            object.frame = { x: 0, speed: 0, start: 0, end: 3}
-            boolleftrun=false;
+        if (boolleftrun) {
+            object.frame = { x: 0, start: 0, end: 3 }
+            boolleftrun = false;
+
         }
         object.charimg.src = "images/runleft.png";
 
@@ -120,7 +125,7 @@ document.onkeydown = (e) => {
         object.velocity.x = 5;
         object.charimg.src = "images/runright.png";
         if (boolrightrun) {
-            object.frame = { x: 0, speed: 0, start: 0, end: 3}
+            object.frame = { x: 0, start: 0, end: 3 }
             boolrightrun = false
         }
     }
@@ -131,16 +136,16 @@ document.onkeyup = (e) => {
     if (e.key === 'ArrowUp') {
         object.velocity.y = 0;
         // } else if (e.key === 'ArrowDown') {
-        //     object.radius=10;
+        // 
     } else if (e.key === 'ArrowLeft') {
         object.velocity.x = 0;
         object.charimg.src = "images/standleft.png";
-        object.frame = { x: 0, speed: 0, start: 0, end: 1}
-        boolleftrun=true;
+        object.frame = { x: 0, start: 0, end: 1 }
+        boolleftrun = true;
     } else if (e.key === 'ArrowRight') {
         object.velocity.x = 0;
         object.charimg.src = "images/standright.png";
-        object.frame = { x: 0, speed: 0, start: 0, end: 1}
-        boolrightrun=true;
+        object.frame = { x: 0, start: 0, end: 1 }
+        boolrightrun = true;
     }
 }
